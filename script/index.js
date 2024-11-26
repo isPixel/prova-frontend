aea49788ca94470895edd04cb08e171dd9820c8d
VINÍCIUS ALMEIDA LACERDA <vinicius.alacerda@a.ucb.br>
2024-11-26 10:34:41 -0300

    Ini

---
+++ script/index.js
@@ -0,0 +1,8 @@
+let btnMenu = document.getElementById('div_hamburguer');
+btnMenu.addEventListener('click', menu);
+let nav = document.getElementById('nav');
+
+
+// function menu() {
+//     nav.classList.add('')
+// }
